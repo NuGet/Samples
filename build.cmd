49a1c9c8a495c33bdc33c51c2b1e0d6791fe2b02
unknown <jeffhand@JEFFHANDMOON.redmond.corp.microsoft.com>
2012-04-27 14:47:01 -0700

    Updated build script to use the nuget.exe in the repository rather than relying on nuget to be in PATH.

--- build.cmd
+++ build.cmd
@@ -3,14 +3,19 @@
 REM Build ClassLibrary
 %WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild SatellitePackageSample\ClassLibrary\ClassLibrary.csproj
 
+SETLOCAL
+SET nuget=SatellitePackageSample\.nuget\NuGet.exe
+
 REM Build the NuGet packages from ClassLibrary
-nuget pack SatellitePackageSample\ClassLibrary\ClassLibrary.nuspec
-nuget pack SatellitePackageSample\ClassLibrary\ClassLibrary.ja-jp.nuspec
-nuget pack SatellitePackageSample\ClassLibrary\ClassLibrary.ru-ru.nuspec
-nuget pack SatellitePackageSample\ClassLibrary\ClassLibrary.Localization.nuspec
+%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.nuspec
+%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.ja-jp.nuspec
+%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.ru-ru.nuspec
+%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.Localization.nuspec
 
 REM Make those packages available through a new feed source called SatellitePackageSample
-nuget sources add -Name SatellitePackageSample -Source "%CD%"
+%nuget% sources add -Name SatellitePackageSample -Source "%CD%"
 
 REM Build the ConsoleApp
 %WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild SatellitePackageSample\ConsoleApp\ConsoleApp.csproj
+
+ENDLOCAL
