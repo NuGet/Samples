d1986d3c82ef5f9573a270e89cf4da35c32bddfd
Xavier Decoster <xavier.decoster@outlook.com>
2016-09-28 18:27:38 +0200

    Processed code review feedback

--- VsCredentialProvider/NuGet.Samples.VisualStudioCredentialProvider/SampleCredentialProvider.cs
+++ VsCredentialProvider/NuGet.Samples.VisualStudioCredentialProvider/SampleCredentialProvider.cs
@@ -10,7 +10,6 @@ using NuGet.VisualStudio;
 
 namespace NuGet.Samples.VisualStudioCredentialProvider
 {
-
     [Export(typeof(IVsCredentialProvider))]
     public sealed class SampleCredentialProvider
         : IVsCredentialProvider
@@ -18,24 +17,60 @@ namespace NuGet.Samples.VisualStudioCredentialProvider
         /// <summary>
         /// Get credentials for the supplied package source Uri.
         /// </summary>
-        /// <param name="uri">The NuGet package source Uri for which credentials are being requested. Implementors are
-        /// expected to first determine if this is a package source for which they can supply credentials.
-        /// If not, then Null should be returned.</param>
-        /// <param name="proxy">Web proxy to use when comunicating on the network.  Null if there is no proxy
+        /// <param name="uri">The NuGet package source Uri for which credentials are being requested.</param>
+        /// <param name="proxy">Web proxy to use when comunicating on the network. Null if there is no proxy
         /// authentication configured.</param>
-        /// <param name="isProxyRequest">True if if this request is to get proxy authentication
-        /// credentials. If the implementation is not valid for acquiring proxy credentials, then
+        /// <param name="isProxyRequest">True if this request is to get proxy authentication
+        /// credentials. If the credential provider does not support acquiring proxy credentials, then
         /// null should be returned.</param>
         /// <param name="isRetry">True if credentials were previously acquired for this uri, but
         /// the supplied credentials did not allow authorized access.</param>
         /// <param name="nonInteractive">If true, then interactive prompts must not be allowed.</param>
         /// <param name="cancellationToken">This cancellation token should be checked to determine if the
         /// operation requesting credentials has been cancelled.</param>
-        /// <returns>Credentials acquired by this provider for the given package source uri.
+        /// <returns>
+        /// Credentials acquired by this provider for the given package source uri.
         /// If the provider does not handle requests for the input parameter set, then null should be returned.
-        /// If the provider does handle the request, but cannot supply credentials, an exception should be thrown.</returns>
-        public async Task<ICredentials> GetCredentialsAsync(Uri uri, IWebProxy proxy, bool isProxyRequest, bool isRetry, bool nonInteractive, CancellationToken cancellationToken)
+        /// If the provider does handle the request, but cannot supply credentials, an exception should be thrown.
+        /// </returns>
+        public async Task<ICredentials> GetCredentialsAsync(
+            Uri uri,
+            IWebProxy proxy,
+            bool isProxyRequest,
+            bool isRetry,
+            bool nonInteractive,
+            CancellationToken cancellationToken)
         {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            // This sample credential provider doesn't handle getting proxy credentials.
+            if (isProxyRequest)
+            {
+                return null;
+            }
+
+            // This sample credential provider does not support a relative Uri.
+            if (!uri.IsAbsoluteUri)
+            {
+                return null;
+            }
+
+            // Force HTTPS
+            if (string.Equals(uri.Scheme, Uri.UriSchemeHttp, StringComparison.InvariantCultureIgnoreCase))
+            {
+                uri = ForceHttps(uri);
+            }
+
+            // This should be enforced HTTPS by now.
+            // If not, the endpoint protocol is not supported by this sample credential provider.
+            if (!string.Equals(uri.Scheme, Uri.UriSchemeHttps, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return null;
+            }
+
             try
             {
                 var credentials = await Authenticate(uri);
@@ -55,19 +90,24 @@ namespace NuGet.Samples.VisualStudioCredentialProvider
             return null;
         }
 
-        private Task<SampleCredentials> Authenticate(Uri uri)
+        private static Uri ForceHttps(Uri uri)
         {
-            if (uri == null)
-            {
-                throw new ArgumentNullException(nameof(uri));
-            }
+            var uriBuilder = new UriBuilder(uri);
+            uriBuilder.Scheme = Uri.UriSchemeHttps;
 
-            // todo: validate if the uri is a valid server uri
+            // Ensure we preserve original port number if provided.
+            var hadDefaultPort = uriBuilder.Uri.IsDefaultPort;
+            uriBuilder.Port = hadDefaultPort ? -1 : uriBuilder.Port;
 
-            // todo: actually authenticate against this specific server (e.g. OAuth flow, Federated Identity handshake)
-            // Result of this operation should provide you with a username and secure (temporary) access token.
+            return uriBuilder.Uri;
+        }
 
-            const string accessToken = "secret";
+        private static Task<SampleCredentials> Authenticate(Uri uri)
+        {
+            // This sample credential provider will randomly generate a new access token.
+            // Replace this with actual authentication against the targeted Uri.
+            // The result of this operation should provide you with a username and a secure (temporary) access token.
+            var accessToken = Guid.NewGuid().ToString();
             const string username = "username";
 
             var token = new SecureString();
