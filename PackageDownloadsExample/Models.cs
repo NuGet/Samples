616e12f0f13cf9471bac8759704dafb71cdbd8a8
Loïc Sharma <sharma.loic@gmail.com>
2020-01-23 13:35:23 -0800

    Add package downloads sample (#37)

---
+++ PackageDownloadsExample/Models.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PackageDownloadsExample
+{
+    /// <summary>
+    /// A package that matched a search query.
+    /// 
+    /// See https://docs.microsoft.com/en-us/nuget/api/search-query-service-resource#search-result
+    /// </summary>
+    public class SearchResult
+    {
+        /// <summary>
+        /// The ID of the matched package.
+        /// </summary>
+        [JsonProperty("id")]
+        public string PackageId { get; set; }
+
+        /// <summary>
+        /// The total downloads for all versions of the matched package.
+        /// </summary>
+        [JsonProperty("totalDownloads")]
+        public long TotalDownloads { get; set; }
+
+        /// <summary>
+        /// The versions of the matched package.
+        /// </summary>
+        [JsonProperty("versions")]
+        public IReadOnlyList<SearchResultVersion> Versions { get; set; }
+    }
+
+    /// <summary>
+    /// A single version from a <see cref="SearchResult"/>.
+    /// 
+    /// See https://docs.microsoft.com/en-us/nuget/api/search-query-service-resource#search-result
+    /// </summary>
+    public class SearchResultVersion
+    {
+        /// <summary>
+        /// The package's full NuGet version after normalization, including any SemVer 2.0.0 build metadata.
+        /// </summary>
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        /// <summary>
+        /// The downloads for this single version of the matched package.
+        /// </summary>
+        [JsonProperty("downloads")]
+        public long Downloads { get; set; }
+    }
+}
---
+++ PackageDownloadsExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+
+namespace PackageDownloadsExample
+{
+    /// <summary>
+    /// Find the download count for each version of a package.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Find the download count for each version of a package.
+        /// </summary>
+        /// <param name="packageId">The package ID. Example: "newtonsoft.json"</param>
+        public static async Task Main(string packageId)
+        {
+            if (string.IsNullOrEmpty(packageId))
+            {
+                Console.WriteLine("The --package-id option is required. Run --help for more information");
+                return;
+            }
+
+            var source = new PackageSource("https://api.nuget.org/v3/index.json");
+            var providers = Repository.Provider.GetCoreV3();
+            var repository = new SourceRepository(source, providers);
+
+            var search = await repository.GetResourceAsync<RawSearchResourceV3>();
+            var filter = new SearchFilter(includePrerelease: true);
+
+            var response = await search.Search($"packageid:{packageId}", filter, skip: 0, take: 20, NullLogger.Instance, CancellationToken.None);
+            var results = response.Select(result => result.ToObject<SearchResult>()).ToList();
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine($"Could not find any results for package ID '{packageId}'");
+                return;
+            }
+
+            foreach (var result in results)
+            {
+                Console.WriteLine($"Package {result.PackageId} has {result.TotalDownloads} total downloads.");
+                Console.WriteLine();
+
+                foreach (var version in result.Versions)
+                {
+                    Console.WriteLine($"Version {version.Version} has {version.Downloads} downloads");
+                }
+            }
+        }
+    }
+}
