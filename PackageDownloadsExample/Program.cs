7b3100ac2df00cabce038bd445c7ff0678e5f0da
Loic Sharma <sharma.loic@gmail.com>
2020-01-23 13:31:45 -0800

    Clean up usings

--- PackageDownloadsExample/Program.cs
+++ PackageDownloadsExample/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Protocol;
