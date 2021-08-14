e25c83b954c0876b89fbcd4e608d9f4a115f84a3
Nikolche Kolev <nikolce1kolev@gmail.com>
2021-08-13 18:06:33 -0700

    Add a package namespaces example (#47)

---
+++ PackageNamespacesExample/App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
